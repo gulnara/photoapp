5bbff59f36c3dca7ec7c1cbfc630e8a36d44850a
Gulnara Mirzakarimova <gulnara@gulnara.me>
2013-06-10 12:23:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoapp::Application.config.secret_token = '108efc85feef202486293d5c19330afeb6c5228545f9e33c23381f94828a814444d0f6d6906f3f1786082291065d4ab2b2f784ea843977bde4c80de1f7088be5'
